d4e75cb788d07ec53199b7fb8451dc0d538ed923
Paul Hellweg <leonhard.hellweg@gmail.com>
2019-08-12 09:57:04 +0200

    connected with  database

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const axios = require("axios");
+const bodyParser = require("body-parser");
 
 const app = express();
 
 //axios  Middleware
-app.use(axios.json());
+app.use(bodyParser.json());
 
+//DB
+const db = require("./config/keys").mongoURI;
+
+mongoose
+  .connect(db)
+  .then(() => console.log("Connected..."))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started  on ${port}`));
